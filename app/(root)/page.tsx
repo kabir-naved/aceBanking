9102ced530a6c48cbc943d3a34de7f33bae86a93
code <kabir.naved.khan@gmail.com>
2025-01-29 23:47:05 +0530

    header are done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
+    const loggedIn = {firstName: "Kabir"};
   return (
     <>
-      <h1 className="bg-red-800">Hello</h1>
+      <section className="home ">
+        <div className="home-content">
+            <header className="home-header">
+
+              {/* HeaderBox component */}
+              <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName}
+                subtext="Access and manage your account and transactions with ease and security from anywhere."
+              />
+              {/* HeaderBox component */}
+              <TotalBalanceBox 
+                accounts= {[]}
+                totalBanks= {1000000}
+                totalCurrentBalance= {1000000}
+              />
+            </header>
+        </div>
+      </section>
     </>
   );
 }
